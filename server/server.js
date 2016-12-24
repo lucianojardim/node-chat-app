cf7c2afb5df71db328d1a42b7cce332210350b55
Luciano Jardim <Luciano Jardim>
2016-12-24 07:55:18 -0600

    Created generateMessage and test for it 201612240755

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const port = process.env.PORT;
 const publicPath = path.join(__dirname + '/../public');
 var app = express();
@@ -19,24 +21,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'Somebody new joined the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
 
   socket.on('createMessage', (message) => {
     console.log('CreateMessage', message);
-    io.emit('newMessage',{ //io.emit broadcast message including the emitter
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    //io.emit broadcast message including the emitter
+    io.emit('newMessage',generateMessage(message.from,message.text));
     // io.broadcast.emit('newMessage',{ //io.emit broadcast message but exclude the emitter
     //   from: message.from,
     //   text: message.text,
