5f31d581f1f2fe6a275ee29cc690f3ffd480e770
Luciano Jardim <Luciano Jardim>
2016-12-24 06:22:57 -0600

    Add greet new user and alert others 201612240622

--- server/server.js
+++ server/server.js
@@ -19,19 +19,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'luciano@example.com',
-  //   text: 'Hey, what is going on?',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'Somebody new joined the chat app',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('CreateMessage', message);
-    io.emit('newMessage',{ //io.emit broadcast message
+    io.emit('newMessage',{ //io.emit broadcast message including the emitter
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // io.broadcast.emit('newMessage',{ //io.emit broadcast message but exclude the emitter
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
