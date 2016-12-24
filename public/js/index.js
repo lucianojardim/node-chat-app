3fc21d47c2f697a0380b21bc8a0e56843209d9f8
Luciano Jardim <Luciano Jardim>
2016-12-24 05:47:22 -0600

    Add newMessage and createMessage events 201612240547

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () { //connect is a standard event
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'viviane@example.com',
+    text: 'Message from client.'
+  })
+});
+
+socket.on('disconnect', function () { //disconnect is a standards event
+  console.log('Disconnected from server');
+})
+
+socket.on('newMessage', function (message) { //newMessage is a custom event
+  console.log('New message', message);
+})
+
--- server/server.js
+++ server/server.js
@@ -19,12 +19,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'luciano@example.com',
+    text: 'Hey, what is going on?',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('CreateMessage', message);
+  })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   })
 });
-
-
 
 server.listen(port, () => {
   console.log(`Started up at port ${port}`);
